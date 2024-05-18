aac8a4d0615156fe6ff05c0c24c85a9872e0f0df
Ekankaar Khera <112088205+ZaRobot10@users.noreply.github.com>
2024-05-18 20:02:46 +0530

    Delete notepad.js

--- notepad.js
+++
@@ -1,6 +0,0 @@
-const clearButton = document.getElementById('clear-button');
-const notepad = document.getElementById('notepad');
-
-clearButton.addEventListener('click', () => {
-  notepad.value = ''; // Clear the content of the notepad
-});
