bd5f63baad6c4b518ce8b50e50184fd01ca27c08
Ekankaar Khera <112088205+ZaRobot10@users.noreply.github.com>
2024-05-18 20:02:40 +0530

    Delete consult.js

--- consult.js
+++
@@ -1,7 +0,0 @@
-var typed2 = new Typed(".text1" , {
-    strings:["not Okay?", "Depressed?", "Demotivated?", "Anxious?", "Suicidal?"],
-    typeSpeed: 100,
-    backSpeed: 100, 
-    backDelay: 1000, 
-    loop: true
-});
